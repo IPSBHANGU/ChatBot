50167f1725a8cf41118cc3f8534aef4c282e8f87
IPSBHANGU <inderpreetbhangu2@gmail.com>
2024-04-02 08:45:45 +0530

    remove unused func

--- Chatbot/Chat/Messages/Model/ChatModel.swift
+++ Chatbot/Chat/Messages/Model/ChatModel.swift
@@ -53,22 +53,6 @@ class ChatModel: NSObject {
         return conversationID
     }
     
-    func checkForUserRelation(conversationID: String, currentUserID: String) -> Bool{
-            let userIDs = conversationID.components(separatedBy: "_")
-            
-            guard userIDs.count == 2 else {
-                return false
-            }
-            
-            if userIDs.first == currentUserID {
-                return true
-            } else if userIDs.last == currentUserID {
-                return true
-            } else {
-                return false
-            }
-        }
-    
     /**
      func getOtherUserID(conversationID: String, currentUserID: String) -> String?
      - Note: Used to get other user UID
